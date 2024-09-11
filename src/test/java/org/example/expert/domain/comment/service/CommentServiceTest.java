b6871dfde8ca1f59c72e49a1529b3371018d71c7
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-09-11 23:03:35 +0900

    10번 CommentServiceTest 일정정상조회 테스트 추가

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -1,14 +1,15 @@
 package org.example.expert.domain.comment.service;
 
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.exception.InvalidRequestException;
-import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
+import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
@@ -72,4 +76,41 @@ class CommentServiceTest {
         // then
         assertNotNull(result);
     }
+
+    @Test
+    public void comment_정상조회() {
+        // given
+        long todoId = 1;
+        CommentSaveRequest request = new CommentSaveRequest("contents");
+
+        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+        User user = User.fromAuthUser(authUser);
+        Todo todo = new Todo("title", "title", "contents", user);
+        Comment comment = new Comment(request.getContents(), user, todo);
+
+        AuthUser authUser1 = new AuthUser(2L, "email2", UserRole.USER);
+        User user1 = User.fromAuthUser(authUser1);
+        Comment comment1 = new Comment(request.getContents(), user1, todo);
+
+        // Mock Repository
+        List<Comment> mockCommentList = List.of(
+                comment, comment1
+        );
+        when(commentRepository.findByTodoIdWithUser(todoId)).thenReturn(mockCommentList);
+
+        // when
+        List<CommentResponse> dtoList = commentService.getComments(todoId);
+
+        // then
+        assertNotNull(dtoList);
+        assertEquals(2, dtoList.size());
+
+        assertEquals("contents", dtoList.get(0).getContents());
+        assertEquals("email", dtoList.get(0).getUser().getEmail());
+
+        assertEquals("contents", dtoList.get(1).getContents());
+        assertEquals("email2", dtoList.get(1).getUser().getEmail());
+    }
+
+
 }
